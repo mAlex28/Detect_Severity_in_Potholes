d5bfdb9d0bf69f5ec1eec6309d12eea4a6fecf97
mAlex28 <alexthegeek2001@gmail.com>
2025-01-21 11:57:21 +0000

    resizing images and testing

--- severity_detection.py
+++ severity_detection.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw
 import xml.etree.ElementTree as ET # Parse xml files
 
-# Import directories
+# Load directories
 images_dir  = 'dataset/IMG/'
 annotation_dir = 'dataset/XML/'
 
@@ -32,10 +32,35 @@ def parse_voc_annotation(annotation_dir, image_dir):
             ymax = int(neighbor.find('ymax').text)
             boxes.append((xmin, ymin, xmax, ymax))
 
-        dataset.append({"Image": image_path, "boxes": boxes})
+        dataset.append({"image": image_path, "boxes": boxes})
 
     return dataset
 
-# Load dataset
+# Create dataset
 dataset = parse_voc_annotation(annotation_dir, images_dir)
-print(dataset)
+
+# Preprocess data
+def preprocess_image(image_path, boxes, target_size=(416,416)):
+    image = Image.open(image_path)
+    height, width = image.size
+
+    # Resize images
+    resized_images = image.resize(target_size)
+    scale_x = target_size[0] / width
+    scale_y = target_size[0] / height
+
+    # Adjust bounding boxes
+    resized_boxes = []
+    for (xmin, ymin, xmax, ymax) in boxes:
+        xmin = int(xmin * scale_x)
+        ymin = int(ymin * scale_y)
+        xmax = int(xmax * scale_x)
+        ymax = int(ymax * scale_y)
+        resized_boxes.append((xmin, ymin, xmax, ymax))
+
+    return resized_images, resized_boxes
+
+image_path = dataset[0]["image"]
+boxes = dataset[0]["boxes"]
+resized_image, resized_boxes = preprocess_image(image_path, boxes)
+
